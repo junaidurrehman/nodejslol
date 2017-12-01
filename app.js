86cb0b597440ca98786b8d0caca477b84d828f3b
junaidranaphp <junaidranaphp@users.noreply.github.com>
2017-12-01 09:33:52 -0800

    hahahaha

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var hbs = require('hbs');
+const port = process.env.PORT || 3000;
 hbs.registerPartials('views/partials');
 app.set('view engine','hbs');
 app.use(express.static('public'));
@@ -9,4 +10,5 @@ app.get('/',(req,res) => {
   res.render('template');
 });
 
-app.listen(3000);
+app.listen(port);
+console.log(port + ' port running');
