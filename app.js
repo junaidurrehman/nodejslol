f55ec046bf6557822e66a6c9b8005e5d4a2ceee3
junaidranaphp <junaidranaphp@users.noreply.github.com>
2017-12-01 11:13:01 -0800

    'bitching'

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
 var hbs = require('hbs');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 hbs.registerPartials('views/partials');
 app.set('view engine','hbs');
 app.use(express.static('public'));
 
 app.get('/',(req,res) => {
   res.render('template');
+});
+app.get('/hello',(req,res) => {
+  res.render('projects',{
+    name:'junaid'
+  });
 });
 
 app.listen(port);
